6a642870d51ea88802b90c43c1f07af14c6dd207
Jacqueline <65547052+jigavilanes1@users.noreply.github.com>
2023-12-20 21:16:56 -0500

    Update DocenteMateriaRepository.java

--- Gavilanes/src/main/java/ec/edu/espe/examen/Gavilanes/dao/DocenteMateriaRepository.java
+++ Gavilanes/src/main/java/ec/edu/espe/examen/Gavilanes/dao/DocenteMateriaRepository.java
@@ -1,9 +1,11 @@
 package ec.edu.espe.examen.Gavilanes.dao;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import ec.edu.espe.examen.Gavilanes.domain.DocenteMateria;
-import java.util.List;
+import ec.edu.espe.examen.Gavilanes.domain.DocenteMateriaPk;
 
-public interface DocenteMateriaRepository extends JpaRepository<DocenteMateria, String>{
-
+public interface DocenteMateriaRepository extends JpaRepository<DocenteMateria, DocenteMateriaPk>{
+    Optional<DocenteMateria> findByPkCodigoDocenteAndPkCodigoMateria(Integer codigoDocente, Integer codigoMateria);
 }
