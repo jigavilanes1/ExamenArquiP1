dd445c91aea875c97c3cab7c2943ac6064453c48
Jacqueline <65547052+jigavilanes1@users.noreply.github.com>
2023-12-20 21:20:40 -0500

    Update EspecialidadRepository.java

--- Gavilanes/src/main/java/ec/edu/espe/examen/Gavilanes/dao/EspecialidadRepository.java
+++ Gavilanes/src/main/java/ec/edu/espe/examen/Gavilanes/dao/EspecialidadRepository.java
@@ -2,9 +2,8 @@ package ec.edu.espe.examen.Gavilanes.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import ec.edu.espe.examen.Gavilanes.domain.Especialidad;
-import java.util.List;
 
 
 public interface EspecialidadRepository extends JpaRepository<Especialidad, String> {
-    List<Especialidad> findByNombre(String Nombre);
+    Especialidad findByNombre(String nombre);
 }
